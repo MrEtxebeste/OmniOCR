84a69f21e367da054ec695c4d237d58bfe5ababa
NbsInstalaciones <nbs@nearbysensor.com>
2026-02-27 20:47:33 +0100

    migrations etc

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from .extensions import db, migrate
 def create_app():
     app = Flask(__name__)
     
-    # Leer la URL de MariaDB desde el archivo .env
+    # Configuración de MariaDB
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
@@ -13,8 +13,11 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     
+    # IMPORTANTE: Importar los modelos aquí para que Flask-Migrate los vea
+    from . import models 
+    
     @app.route('/ping')
     def ping():
-        return "¡OmniOCR está vivo y conectado a MariaDB!"
+        return "¡Conexión establecida y modelos cargados!"
         
     return app
